41a7aab09fbacdee59c5cbada6d4cabdc2ead168
Smogg <spazzaglia98@gmail.com>
2022-07-28 12:12:13 -0500

    Resolved tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
 
+const repeatString = function(word, times) {
+    if (times<0) return "ERROR";
+    hi = "";
+    for (i=0;i<times;i++){
+        hi += word;
+    }
+    return hi;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(valueOne, valueTwo) {
+    let sum = 0;
+    if (valueOne < 0 || valueTwo < 0){
+        return "ERROR";
+    }
+    else if (!Number.isInteger(valueOne) || !Number.isInteger(valueTwo)) return "ERROR";
+    else if (valueOne < valueTwo) {
+        for (i=valueOne; i<=valueTwo; i++){
+            sum +=i;
+        }
+    }
+    else if (valueOne > valueTwo){
+        
+        for (i=valueTwo; i<=valueOne; i++){
+            sum +=i;
+        }
+    }
+    return sum;
 
 };
 
