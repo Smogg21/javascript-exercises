147eff70dbe66a86ffa9869ae7c93b6774bab4b4
Smogg <spazzaglia98@gmail.com>
2022-07-28 12:25:51 -0500

    Resolved more cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let transform = ((farenheit -32)*(5/9));
+  roundedVar = Math.round(transform * 10)/10;
+  return roundedVar;
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let transform = (celcius*(9/5)) + 32;
+  roundedVar = Math.round(transform*10)/10;
+  return roundedVar;
 
 };
 
