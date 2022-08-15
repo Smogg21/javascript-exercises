70aa6ebb5fb70420eb2b57bfd236c21d65005eec
Smogg <spazzaglia98@gmail.com>
2022-08-14 21:45:33 -0300

    Add Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(a,b) {
+  return (a+b);
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return (a-b);
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((previous, current) => previous + current, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((previous,current)=> previous*current)
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 	
 };
 
